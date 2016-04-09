1b2d125576737fe25b7767339b7e342a778377dd
Bartosz Cieśla <209851@student.pwr.wroc.pl>
2016-04-09 17:28:30 +0200

    Removed debug prints

--- Orienteering/src/main/java/pl/edu/MainController.java
+++ Orienteering/src/main/java/pl/edu/MainController.java
@@ -17,7 +17,6 @@ public class MainController {
 
     @RequestMapping("/")
     public String homepage(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("blablabla2");
         model.addAttribute("sigh", "lesigh");
         return "index";
     }
--- Orienteering/src/main/java/pl/edu/OrienteeringApplication.java
+++ Orienteering/src/main/java/pl/edu/OrienteeringApplication.java
@@ -10,14 +10,6 @@ import java.util.Arrays;
 public class OrienteeringApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(OrienteeringApplication.class, args);
-
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = context.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
+		SpringApplication.run(OrienteeringApplication.class, args);
 	}
 }
