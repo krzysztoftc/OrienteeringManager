97855c815bfd65ffa764470ef474f8eba198993e
Bartosz Cieśla <209851@student.pwr.wroc.pl>
2016-05-17 16:30:11 +0200

    Base for competitor form and validator

---
+++ Orienteering/src/main/java/pl/edu/controller/form/CompetitorValidator.java
@@ -0,0 +1,32 @@
+package pl.edu.controller.form;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import pl.edu.service.user.IUserService;
+import pl.edu.validator.CommonValidator;
+
+public class LoginValidator extends CommonValidator {
+
+	@Autowired
+	IUserService userService;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return clazz.equals(LoginForm.class);
+	}
+
+	@Override
+	public void validateForm(Object target, Errors errors) {
+
+		LoginForm form = (LoginForm) target;
+
+		if (StringUtils.isBlank(form.getUser().getEmail())) {
+			errors.rejectValue("user.email", "user.email.cannot.be.null");
+		}
+
+		if (StringUtils.isBlank(form.getUser().getPassword())) {
+			errors.rejectValue("user.password", "user.password.cannot.be.null");
+		}
+	}
+}
