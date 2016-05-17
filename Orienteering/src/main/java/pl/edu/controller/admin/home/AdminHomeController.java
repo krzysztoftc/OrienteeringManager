fd285435134f8c97c384fa3bf7e28e9560931e9b
Bartosz Cieśla <209851@student.pwr.wroc.pl>
2016-05-17 18:45:23 +0200

    Changed model attribute as annotation

--- Orienteering/src/main/java/pl/edu/controller/admin/home/AdminHomeController.java
+++ Orienteering/src/main/java/pl/edu/controller/admin/home/AdminHomeController.java
@@ -1,13 +1,18 @@
 package pl.edu.controller.admin.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+import pl.edu.controller.competitor.form.CompetitorForm;
 import pl.edu.model.competitor.Competitor;
 import pl.edu.repository.competitor.Competitors;
+import pl.edu.repository.user.Users;
 import pl.edu.service.competitor.ICompetitorService;
 
 import javax.servlet.ServletContext;
@@ -25,10 +30,18 @@ public class AdminHomeController {
     @Autowired
     private ICompetitorService competitorService;
 
+    @ModelAttribute("competitorForm")
+    public CompetitorForm form() {
+        return new CompetitorForm();
+    }
+
+    @ModelAttribute("competitors")
+    public List<Competitor> competitorList() {
+        return competitorService.list(Competitors.findAll());
+    }
+
     @RequestMapping(value = {"/admin", "/admin/"})
     public String home(ModelMap model){
-        List<Competitor> competitors = competitorService.list(Competitors.findAll());
-        model.addAttribute("competitors", competitors);
         return "admin/index";
     }
 }
--- Orienteering/src/main/java/pl/edu/controller/clubs/home/ClubsHomeController.java
+++ Orienteering/src/main/java/pl/edu/controller/clubs/home/ClubsHomeController.java
@@ -1,19 +1,18 @@
 package pl.edu.controller.clubs.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.controller.competitor.form.CompetitorForm;
 import pl.edu.model.competitor.Competitor;
 import pl.edu.repository.competitor.Competitors;
 import pl.edu.repository.user.Users;
 import pl.edu.service.competitor.ICompetitorService;
 import pl.edu.service.user.IUserService;
-
 import java.util.List;
 
 /**
@@ -28,12 +27,20 @@ public class ClubsHomeController {
     @Autowired
     private ICompetitorService competitorService;
 
-    @RequestMapping(value = {"/clubs", "/clubs/"})
-    public String home(Model model){
+    @ModelAttribute("competitorForm")
+    public CompetitorForm form() {
+        return new CompetitorForm();
+    }
+
+    @ModelAttribute("competitors")
+    public List<Competitor> competitorList() {
         User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Long clubId = userService.uniqueObject(Users.findAll().withEmail(user.getUsername())).getClubId();
-        List<Competitor> competitors = competitorService.list(Competitors.findAll().withClubId(clubId));
-        model.addAttribute("competitors", competitors);
+        return competitorService.list(Competitors.findAll().withClubId(clubId));
+    }
+
+    @RequestMapping(value = {"/clubs", "/clubs/"})
+    public String home(){
         return "clubs/index";
     }
 
