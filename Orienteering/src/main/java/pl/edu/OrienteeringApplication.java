b64debbe58f6d9b950d28ed86a784d3797dc59b3
Krzysztof Cabala <210047@student.pwr.wroc.pl>
2016-05-25 23:30:54 +0200

    assets in login + first styles try in admin.index

--- Orienteering/src/main/java/pl/edu/OrienteeringApplication.java
+++ Orienteering/src/main/java/pl/edu/OrienteeringApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-public class OrienteeringApplication {
+public class OrienteeringApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 //		for(String s : args)
@@ -16,4 +18,11 @@ public class OrienteeringApplication {
 //		new ClassPathXmlApplicationContext("context.xml");
 		SpringApplication.run(OrienteeringApplication.class, args);
 	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		if (!registry.hasMappingForPattern("/assets/**")) {
+//			registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+//		}
+//	}
 }
