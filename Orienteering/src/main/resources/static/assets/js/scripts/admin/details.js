657e9cef4cc8f74e97985192bf5995610266c48d
Krzysztof Cabała <krzysztoftc@vp.pl>
2016-05-26 23:05:50 +0200

    better view

--- Orienteering/src/main/resources/static/assets/js/scripts/admin/details.js
+++ Orienteering/src/main/resources/static/assets/js/scripts/admin/details.js
@@ -3,12 +3,5 @@
  */
 
 function toogle(id) {
-    console.log(id);
-    console.log($("#option"+id));
    $("#option"+id).slideToggle("fast");
 }
-
-$().ready(function () {
-    $(".option").hide();
-   
-});
