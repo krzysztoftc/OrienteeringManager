0b497ba78deb6ba71a2655a5a2d5c8919c478947
Bartosz Cieśla <209851@student.pwr.wroc.pl>
2016-05-29 12:14:22 +0200

    Added logout handling

--- Orienteering/src/main/java/pl/edu/controller/login/LoginController.java
+++ Orienteering/src/main/java/pl/edu/controller/login/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +18,7 @@ import pl.edu.controller.login.form.LoginForm;
 import pl.edu.controller.login.form.LoginValidator;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -83,8 +85,11 @@ public class LoginController extends BaseController {
     }
 
     @RequestMapping(value = { "/logout", "/logout/" }, method = RequestMethod.POST)
-    public String logout(Model model, LoginForm loginForm, BindingResult result, HttpServletRequest request) {
-        return "login";
+    public String logout(BindingResult result, HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
-
 }
