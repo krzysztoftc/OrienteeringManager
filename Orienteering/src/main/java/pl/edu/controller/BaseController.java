726bb03e9e2547b5def808e89fe5aec7a9adf2dd
Bartosz Cieśla <209851@student.pwr.wroc.pl>
2016-05-31 14:39:12 +0200

    Changed way of displaying navbar info

--- Orienteering/src/main/java/pl/edu/controller/BaseController.java
+++ Orienteering/src/main/java/pl/edu/controller/BaseController.java
@@ -1,8 +1,8 @@
 package pl.edu.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,16 +18,35 @@ public abstract class BaseController {
     @Autowired
     IUserService userService;
 
-    @ModelAttribute("clubData")
-    public Club clubData(){
+    @ModelAttribute("loggedUsername")
+    public pl.edu.model.user.User loggedUsername(){
+
+        pl.edu.model.user.User loggedUsername = null;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         try {
             User userDetails = (User) authentication.getPrincipal();
-            return userService.uniqueObject(Users.findAll().withEmail(userDetails.getUsername())).getClub();
+            loggedUsername = userService.uniqueObject(Users.findAll().withEmail(userDetails.getUsername()));
         }
-        catch(ClassCastException e){
+        catch(Exception e){
         }
-        return new Club();
-//        return user;
+
+        if( loggedUsername == null)
+            loggedUsername = new pl.edu.model.user.User();
+
+        if(loggedUsername.getType() == null)
+            loggedUsername.setType("NONE");
+
+        if(loggedUsername.getClub() == null){
+            Club club = new Club();
+            loggedUsername.setClub(club);
+        }
+
+        if(StringUtils.isBlank(loggedUsername.getClub().getAgentName())){
+            loggedUsername.getClub().setAgentName("");
+        }
+
+        loggedUsername.setPassword("");
+
+        return loggedUsername;
     }
 }
